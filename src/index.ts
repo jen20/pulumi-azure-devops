41b2c2cf601706adcd218fcaa6a9e8c6a148bf7d
James Nugent <james@jen20.com>
2018-10-02 08:27:16 +0200

    Make trivial change

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const bucket = new aws.s3.Bucket("test-bucket");
 
 // Export the DNS name of the bucket
 export const bucketName = bucket.bucketDomainName;
